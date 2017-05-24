eb018c419ce206c2b8ce7450e504515a3508183b
David Gray <davidgray@Davids-MBP.devel>
2017-05-24 16:31:10 -0600

    Got the songs to play and started on the images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,42 @@
-//create initial vars and arrarys
+//create initial variables and arrarys
 var wins = 0;
 var guessesLeft = 10;
 var guessedLetters = [];
+
+//variables for the audio tracks
+var audio0 = new Audio('assets/sounds/cemetary_gates.mp3');
+var audio1 = new Audio('assets/sounds/master_of_puppets.mp3');
+var audio2 = new Audio('assets/sounds/chaos_ad.mp3');
+var audio3 = new Audio('assets/sounds/south_of_heaven.mp3');
+var audio4 = new Audio('assets/sounds/breaking_the_law.mp3');
+var audio5 = new Audio('assets/sounds/the_drapery_falls.mp3');
+var audio6 = new Audio('assets/sounds/paranoid.mp3');
+var audio7 = new Audio('assets/sounds/hallowed_be_thy_name.mp3');
+
+//variables for the images
+var img0 = document.createElement("img");
+img0.src = "assets/images/cemetary_gates.jpg";
+
+var img1 = document.createElement("img");
+img1.src = "assets/images/master_of_puppets.jpg";
+
+var img2 = document.createElement("img");
+img2.src = "assets/images/chaos_ad.jpg";
+
+var img3 = document.createElement("img");
+img3.src = "assets/images/south_of_heaven.jpg";
+
+var img4 = document.createElement("img");
+img4.src = "assets/images/breaking_the_law.jpg";
+
+var img5 = document.createElement("img");
+img5.src = "assets/images/the_drapery_falls.jpg";
+
+var img6 = document.createElement("img");
+img6.src = "assets/images/paranoid.jpg";
+
+var img7 = document.createElement("img");
+img7.src = "assets/images/hallowed_be_thy_name.jpg";
 
 //Create an array of hangman content
 var songs = ["cemetary gates", "master of puppets", "chaos ad", "south of heaven", "breaking the law",
@@ -41,13 +76,50 @@ for(var k = 0; k < split.length; k++) {
 	}
 }
 if (allLettersGuessed){
+	if (randomSong === songs[0]) {
+		audio0.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img0);
+		win();
+	} else if (randomSong === songs[1]) {
+		audio1.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img1);
+		win();
+	} else if (randomSong === songs[2]) {
+		audio2.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img2);
+		win(); 
+	} else if (randomSong === songs[3]) {
+		audio3.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img3);
+		win();
+	} else if (randomSong === songs[4]) {
+		audio4.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img4);
+		win();
+	} else if (randomSong === songs[5]) {
+		audio5.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img5);
+		win();
+	} else if (randomSong === songs[6]) {
+		audio6.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img6);
+		win();
+	} else if (randomSong === songs[7]) {
+		audio7.play();
+		document.querySelector('#letters').innerHTML = ('#letters').appendChild(img7);
+		win();
+	}
+}
+
+//Create a function for winning
+function win() {
 	wins++;
 	guessesLeft = 10;
 	guessedLetters.length = 0;
 	filter.length = 0;
-	songs[Math.floor(Math.random()
-	* songs.length)];
+	randomSong = songs[Math.floor(Math.random()* songs.length)];
 }
+
 
 //For loop to check if letters were missed
 for(var h = 0; h < split.length; h++){
